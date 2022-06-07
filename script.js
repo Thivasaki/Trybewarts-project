c9b00c0c1b3070f726b9d0717ed6f2ee6f9db780
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-06-07 16:13:40 -0300

    Requisito 3 OK

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-1
+const button = document.getElementById('btn-submit');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+function alertSubmit() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    return alert('Olá, Tryber!');
+  }
+  return alert('Email ou senha inválidos.');
+}
+
+button.addEventListener('click', alertSubmit);
